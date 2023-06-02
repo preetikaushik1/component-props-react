7005620829e2b4846ecc6a255deaeb1374a6a46a
preeti1098 <124133826+preeti1098@users.noreply.github.com>
2023-06-02 14:39:19 +0530

    contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -17,7 +17,7 @@ function Contact(){
               <div className="hr">preetikaushik10@gmail.com</div>
             </div>
             <div className="box">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563977.441819753!2d73.39667031648943!3d29.269210292384642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390db00b8670400b%3A0x732eaab4aaa6c143!2sHaryana!5e0!3m2!1sen!2sin!4v1685694063187!5m2!1sen!2sin" width="100%" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
+            <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563977.441819753!2d73.39667031648943!3d29.269210292384642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390db00b8670400b%3A0x732eaab4aaa6c143!2sHaryana!5e0!3m2!1sen!2sin!4v1685694063187!5m2!1sen!2sin" width="100%" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
             </div>
         </div>
         </div>
